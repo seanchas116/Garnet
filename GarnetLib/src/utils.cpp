d8adafd5416a0db465ed67b4e68da3f5a38c869c
Ryohei Ikegami <iofg2100@gmail.com>
2014-01-09 23:24:27 +0900

    Refactor method call in utils.cpp

--- GarnetLib/src/utils.cpp
+++ GarnetLib/src/utils.cpp
@@ -6,6 +6,181 @@
 
 namespace Garnet {
 
+namespace {
+
+class MethodCall
+{
+public:
+    MethodCall(mrb_state *mrb, mrb_value self) :
+        mrb_(mrb),
+        self_(self)
+    {
+        auto ptr = Object::fromValue(self);
+        object_ = ptr.get();
+        meta_object_ = ptr->metaObject();
+        method_name_ = mrb_sym2name(mrb, mrb->c->ci->mid);
+    }
+
+    mrb_value callMethod()
+    {
+        auto mrb = mrb_;
+        mrb_value *argv = nullptr;
+        int argc = 0;
+        mrb_get_args(mrb, "*", &argv, &argc);
+
+        auto found_method = calledMethod();
+
+        if (!found_method.isValid()) {
+            setNoMethodError();
+            return mrb_nil_value();
+        }
+
+        int object_argc = found_method.parameterCount();
+
+        bool has_vlist = (object_argc > 0 && (found_method.parameterType(object_argc - 1) == qMetaTypeId<QVariantList>()));
+        if (has_vlist) object_argc--;
+
+        if (argc < object_argc || (argc > object_argc && !has_vlist)) {
+             setWrongNumberOfArgumentsError(argc, object_argc);
+             return mrb_nil_value();
+        }
+
+        QVariant variants[10];
+        QGenericArgument gargs[10];
+        QVariantList vlist;
+
+        for (int i = 0; i < argc; ++i) {
+            variants[i] = Variant(mrb, argv[i]);
+            if (has_vlist && i >= object_argc) {
+                vlist.push_back(variants[i]);
+            }
+            else {
+                gargs[i] = Q_ARG(QVariant, variants[i]);
+            }
+        }
+
+        if (has_vlist) {
+            gargs[object_argc] = Q_ARG(QVariantList, vlist);
+        }
+
+        int return_type = found_method.returnType();
+
+        void *buffer = nullptr;
+        if (return_type != qMetaTypeId<void>()) {
+            buffer = QMetaType::create(return_type);
+        }
+
+        found_method.invoke(object_, QGenericReturnArgument(QMetaType::typeName(return_type), buffer),
+                            gargs[0], gargs[1], gargs[2], gargs[3], gargs[4],
+                            gargs[5], gargs[6], gargs[7], gargs[8], gargs[9]);
+
+        Variant variant;
+        if (return_type == qMetaTypeId<QVariant>()) {
+            variant = *reinterpret_cast<const QVariant*>(buffer);
+        }
+        else if (return_type != qMetaTypeId<void>()) {
+            variant = Variant(QVariant(return_type, buffer));
+        }
+
+        QMetaType::destroy(return_type, buffer);
+
+        return variant.toValue(mrb);
+    }
+
+    mrb_value accessProperty()
+    {
+        auto mrb = mrb_;
+        bool setter;
+        auto found_property = accessedProperty(&setter);
+
+        if (!found_property.isValid()) {
+            setNoMethodError();
+            return mrb_nil_value();
+        }
+
+        if (setter) {
+            mrb_value value;
+            mrb_get_args(mrb, "o", &value);
+            found_property.write(object_, Variant(mrb, value));
+        }
+
+        return Variant(found_property.read(object_)).toValue(mrb);
+    }
+
+    // this function may prevent destructors of local variables from not being called properly
+    // because mrb_raisef uses longjmp
+    static void handleError(mrb_state *mrb)
+    {
+        if (error.occured) {
+            error.occured = false;
+            mrb_raisef(mrb, error.klass, error.format, error.arg1, error.arg2);
+        }
+    }
+
+private:
+
+    QMetaMethod calledMethod()
+    {
+        for (int i = 0; i < meta_object_->methodCount(); ++i) {
+            QMetaMethod method = meta_object_->method(i);
+            if (strcmp(method_name_, method.name()) == 0) {
+                return method;
+            }
+        }
+        return QMetaMethod();
+    }
+
+    QMetaProperty accessedProperty(bool *is_setter)
+    {
+        QByteArray property_name = method_name_;
+        if (property_name.endsWith("=")) {
+            *is_setter = true;
+            property_name = property_name.mid(0, property_name.size() - 1);
+        } else {
+            *is_setter = false;
+        }
+        return meta_object_->property(meta_object_->indexOfProperty(property_name.data()));
+    }
+
+    void setNoMethodError()
+    {
+        auto mrb = mrb_;
+        error.occured = true;
+        error.klass = E_NOMETHOD_ERROR;
+        error.format = "undefined method '%S' for %S";
+        error.arg1 = mrb_str_new_cstr(mrb, method_name_);
+        error.arg2 = mrb_str_new_cstr(mrb, meta_object_->className());
+    }
+
+    void setWrongNumberOfArgumentsError(int wrong, int correct)
+    {
+        auto mrb = mrb_;
+        error.occured = true;
+        error.klass = E_ARGUMENT_ERROR;
+        error.format = "wrong number of arguments (%S for %S)";
+        error.arg1 = mrb_fixnum_value(wrong);
+        error.arg2 = mrb_fixnum_value(correct);
+    }
+
+    mrb_state *mrb_;
+    mrb_value self_;
+    const char *method_name_;
+    const QMetaObject *meta_object_;
+    QObject *object_;
+
+    struct Error {
+        bool occured = false;
+        RClass *klass;
+        const char *format;
+        mrb_value arg1, arg2;
+    };
+    static Error error;
+};
+
+MethodCall::Error MethodCall::error;
+
+}
+
 void registerMethods(mrb_state *mrb, RClass *garnet_class,const QMetaObject& object)
 {
     for (int i = 0; i < object.methodCount(); ++i) {
@@ -15,82 +190,9 @@ void registerMethods(mrb_state *mrb, RClass *garnet_class,const QMetaObject& obj
             const char *method_name = method.name();
 
             auto method_impl = [](mrb_state *mrb, mrb_value self) -> mrb_value {
-                mrb_value *argv = nullptr;
-                int argc = 0;
-                mrb_get_args(mrb, "*", &argv, &argc);
-
-                const char *method_name = mrb_sym2name(mrb, mrb->c->ci->mid);
-
-                std::shared_ptr<QObject> q_object = Object::fromValue(self);
-                const QMetaObject *object = q_object->metaObject();
-
-                QMetaMethod found_method;
-                for (int i = 0; i < object->methodCount(); ++i) {
-                    QMetaMethod method = object->method(i);
-                    if (strcmp(method_name, method.name()) == 0) {
-                        found_method = method;
-                    }
-                }
-
-                if (!found_method.isValid()) {
-                    mrb_raisef(mrb, E_NOMETHOD_ERROR,
-                               "undefined method '%S' for %S",
-                               mrb_str_new_cstr(mrb, method_name),
-                               mrb_str_new_cstr(mrb, object->className()));
-                }
-
-                int object_argc = found_method.parameterCount();
-
-                bool has_vlist = (object_argc > 0 && (found_method.parameterType(object_argc - 1) == qMetaTypeId<QVariantList>()));
-                if (has_vlist) object_argc--;
-
-                if (argc < object_argc || (argc > object_argc && !has_vlist)) {
-                    mrb_raisef(mrb, E_ARGUMENT_ERROR,
-                               "wrong number of arguments (%S for %S)",
-                               mrb_fixnum_value(argc),
-                               mrb_fixnum_value(object_argc));
-                }
-
-                QVariant variants[10];
-                QGenericArgument gargs[10];
-                QVariantList vlist;
-
-                for (int i = 0; i < argc; ++i) {
-                    variants[i] = Variant(mrb, argv[i]);
-                    if (has_vlist && i >= object_argc) {
-                        vlist.push_back(variants[i]);
-                    }
-                    else {
-                        gargs[i] = Q_ARG(QVariant, variants[i]);
-                    }
-                }
-
-                if (has_vlist) {
-                    gargs[object_argc] = Q_ARG(QVariantList, vlist);
-                }
-
-                int return_type = found_method.returnType();
-
-                void *buffer = nullptr;
-                if (return_type != qMetaTypeId<void>()) {
-                    buffer = QMetaType::create(return_type);
-                }
-
-                found_method.invoke(q_object.get(), QGenericReturnArgument(QMetaType::typeName(return_type), buffer),
-                                    gargs[0], gargs[1], gargs[2], gargs[3], gargs[4],
-                                    gargs[5], gargs[6], gargs[7], gargs[8], gargs[9]);
-
-                Variant variant;
-                if (return_type == qMetaTypeId<QVariant>()) {
-                    variant = *reinterpret_cast<const QVariant*>(buffer);
-                }
-                else if (return_type != qMetaTypeId<void>()) {
-                    variant = Variant(QVariant(return_type, buffer));
-                }
-
-                QMetaType::destroy(return_type, buffer);
-
-                return variant.toValue(mrb);
+                auto result = MethodCall(mrb, self).callMethod();
+                MethodCall::handleError(mrb);
+                return result;
             };
 
             mrb_define_method(mrb, garnet_class, method_name, method_impl, ARGS_NONE());
@@ -104,38 +206,9 @@ void registerMethods(mrb_state *mrb, RClass *garnet_class,const QMetaObject& obj
         const char *property_name = property.name();
 
         auto method_impl = [](mrb_state *mrb, mrb_value self) -> mrb_value {
-            bool setter = false;
-            QString method_name = mrb_sym2name(mrb, mrb->c->ci->mid);
-            if (method_name.endsWith("=")) {
-                setter = true;
-                method_name = method_name.mid(0, method_name.size() - 1);
-            }
-
-            std::shared_ptr<QObject> q_object = Object::fromValue(self);
-            const QMetaObject *object = q_object->metaObject();
-
-            QMetaProperty found_property;
-            for (int i = 0; i < object->propertyCount(); ++i) {
-                QMetaProperty property = object->property(i);
-                if (method_name == property.name()) {
-                    found_property = property;
-                }
-            }
-
-            if (!found_property.isValid()) {
-                mrb_raisef(mrb, E_NOMETHOD_ERROR,
-                           "undefined method '%S' for %S",
-                           mrb_str_new_cstr(mrb, method_name.toUtf8().data()),
-                           mrb_str_new_cstr(mrb, object->className()));
-            }
-
-            if (setter) {
-                mrb_value value;
-                mrb_get_args(mrb, "o", &value);
-                found_property.write(q_object.get(), Variant(mrb, value));
-            }
-
-            return Variant(found_property.read(q_object.get())).toValue(mrb);
+            auto result = MethodCall(mrb, self).accessProperty();
+            MethodCall::handleError(mrb);
+            return result;
         };
 
         QString getter_method_name = property_name;
